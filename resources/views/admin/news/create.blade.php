2a956c3bead57ce2486375d89b3a999b38fb26f1
yuki shiozaki <manten11@gmail.com>
2019-09-27 16:03:49 +0000

    thiree commit

---
+++ resources/views/admin/news/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1">
+        
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+    </body>
+</html>
---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1">
+        
+        <title>MyProfile</title>
+    </head>
+    <body>
+        <h1>MyProfile</h1>
+    </body>
+</html>
